84a80cbc511963227cfbef2641cbabf383628f50
Dimitar Makariev <dmakariev@trustarc.com>
2023-10-14 18:23:39 +0300

    added LinkedList as stack

--- java-core/stack-deque/src/test/java/com/makariev/examples/core/StackDequeExampleTest.java
+++ java-core/stack-deque/src/test/java/com/makariev/examples/core/StackDequeExampleTest.java
@@ -3,6 +3,7 @@ package com.makariev.examples.core;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.LinkedList;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Stack;
@@ -10,7 +11,7 @@ import java.util.Stack;
 public class StackDequeExampleTest {
 
     @Test
-    void testStackAndDequeOperations() {
+    void testStackLinkedListDeque() {
         // Create a stack
         final Stack<String> stack = new Stack<>();
         stack.push("Alan Turing");
@@ -22,14 +23,32 @@ public class StackDequeExampleTest {
         // Check the size of the stack
         assertThat(stack.size()).isEqualTo(5);
 
-        // Pop elements from the stack and check their order
+        // Pop elements from the stack and check their order (FILO)
         assertThat(stack.pop()).isEqualTo("Donald Knuth");
         assertThat(stack.pop()).isEqualTo("Ada Lovelace");
         assertThat(stack.pop()).isEqualTo("Linus Torvalds");
         assertThat(stack.pop()).isEqualTo("Grace Hopper");
         assertThat(stack.pop()).isEqualTo("Alan Turing");
 
-        // Create a deque
+        // Create a LinkedList (used as a stack)
+        final LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.push("Alan Turing");
+        linkedList.push("Grace Hopper");
+        linkedList.push("Linus Torvalds");
+        linkedList.push("Ada Lovelace");
+        linkedList.push("Donald Knuth");
+
+        // Check the size of the linked list (stack)
+        assertThat(linkedList.size()).isEqualTo(5);
+
+        // Remove elements from the linked list (stack) and check their order (FILO)
+        assertThat(linkedList.pop()).isEqualTo("Donald Knuth");
+        assertThat(linkedList.pop()).isEqualTo("Ada Lovelace");
+        assertThat(linkedList.pop()).isEqualTo("Linus Torvalds");
+        assertThat(linkedList.pop()).isEqualTo("Grace Hopper");
+        assertThat(linkedList.pop()).isEqualTo("Alan Turing");
+
+        // Create a Deque (used as a stack)
         final Deque<String> deque = new ArrayDeque<>();
         deque.push("Alan Turing");
         deque.push("Grace Hopper");
@@ -37,10 +56,10 @@ public class StackDequeExampleTest {
         deque.push("Ada Lovelace");
         deque.push("Donald Knuth");
 
-        // Check the size of the deque
+        // Check the size of the deque (stack)
         assertThat(deque.size()).isEqualTo(5);
 
-        // Pop elements from the deque and check their order
+        // Remove elements from the deque (stack) and check their order (FILO)
         assertThat(deque.pop()).isEqualTo("Donald Knuth");
         assertThat(deque.pop()).isEqualTo("Ada Lovelace");
         assertThat(deque.pop()).isEqualTo("Linus Torvalds");
