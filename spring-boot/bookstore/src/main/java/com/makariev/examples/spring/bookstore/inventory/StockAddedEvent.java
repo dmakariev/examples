c778170620c8ae2b61f835577326b68f4834f293
Dimitar Makariev <dimitar.makariev@gmail.com>
2024-05-10 17:08:12 +0300

    added hibernate enhance plugin; improved documentation for publishing events

--- spring-boot/bookstore/src/main/java/com/makariev/examples/spring/bookstore/inventory/StockAddedEvent.java
+++ spring-boot/bookstore/src/main/java/com/makariev/examples/spring/bookstore/inventory/StockAddedEvent.java
@@ -1,9 +1,12 @@
 package com.makariev.examples.spring.bookstore.inventory;
 
+import org.jmolecules.event.annotation.DomainEvent;
+
 /**
  *
  * @author dmakariev
  */
+@DomainEvent
 public record StockAddedEvent(Long inventoryId, int quantityAdded) {
 
 }
--- spring-boot/bookstore/src/main/java/com/makariev/examples/spring/bookstore/inventory/StockRemovedEvent.java
+++ spring-boot/bookstore/src/main/java/com/makariev/examples/spring/bookstore/inventory/StockRemovedEvent.java
@@ -1,9 +1,12 @@
 package com.makariev.examples.spring.bookstore.inventory;
 
+import org.jmolecules.event.annotation.DomainEvent;
+
 /**
  *
  * @author dmakariev
  */
+@DomainEvent
 public record StockRemovedEvent(Long inventoryId, int quantityRemoved) {
 
 }
