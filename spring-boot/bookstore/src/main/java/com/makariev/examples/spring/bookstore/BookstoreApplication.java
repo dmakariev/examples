bd61c0aaa9144052467dafed80bc0b57380c57fe
Dimitar Makariev <dimitar.makariev@gmail.com>
2024-05-28 00:35:36 +0300

    upgraded to spring boot 3.3.0 , added Docker.native and compose-native.yaml  ( unable to use spring-modulith-starter-insight in native app )

--- spring-boot/bookstore/src/main/java/com/makariev/examples/spring/bookstore/BookstoreApplication.java
+++ spring-boot/bookstore/src/main/java/com/makariev/examples/spring/bookstore/BookstoreApplication.java
@@ -1,17 +1,47 @@
 package com.makariev.examples.spring.bookstore;
 
+import jakarta.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.List;
+import org.hibernate.persister.entity.JoinedSubclassEntityPersister;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 /**
  *
  * @author dmakariev
  */
 @SpringBootApplication
+@RegisterReflectionForBinding({JoinedSubclassEntityPersister.class})
 public class BookstoreApplication {
+
+    @Autowired
+    private Environment env;
 
     public static void main(String[] args) {
         SpringApplication.run(BookstoreApplication.class, args);
+    }
+
+    @PostConstruct
+    public void printConfigProperties() {
+        final List<String> propertyKeys = Arrays.asList(
+                "management.endpoints.web.exposure.include",
+                "management.endpoint.env.show-values",
+                "management.tracing.sampling.probability",
+                "management.tracing.enabled",
+                "management.zipkin.tracing.endpoint",
+                "spring.datasource.url",
+                "spring.h2.console.enabled",
+                "spring.h2.console.path",
+                "spring.h2.console.settings.web-allow-others"
+        );
+
+        for (String key : propertyKeys) {
+            System.out.println(key + " = '" + env.getProperty(key) + "'");
+        }
     }
 
 }
