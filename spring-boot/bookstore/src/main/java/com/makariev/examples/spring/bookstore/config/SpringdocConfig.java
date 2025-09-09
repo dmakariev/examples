ae3d111ed4b318257621b487675977ec39568d09
Dimitar Makariev <dimitar.makariev@gmail.com>
2025-09-10 01:38:20 +0300

    added project version to swagger output and git commit to actuator/info

--- spring-boot/bookstore/src/main/java/com/makariev/examples/spring/bookstore/config/SpringdocConfig.java
+++ spring-boot/bookstore/src/main/java/com/makariev/examples/spring/bookstore/config/SpringdocConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,17 +14,19 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class SpringdocConfig {
-    
-  @Bean
-  public OpenAPI springShopOpenAPI() {
-      return new OpenAPI()
-              .info(new Info().title("Bookstore API")
-              .description("Spring Bookstore sample application")
-              .version("v0.0.1")
-              .license(new License().name("MIT").url("https://makariev.com")))
-              .externalDocs(new ExternalDocumentation()
-              .description("Bookstore  Documentation")
-              .url("https://makariev.com"));
-  }
-    
+
+    @Bean
+    public OpenAPI springShopOpenAPI(
+            @Value("${api.version}") String apiVersion
+    ) {
+        return new OpenAPI()
+                .info(new Info().title("Bookstore API")
+                        .description("Spring Bookstore sample application")
+                        .version(apiVersion)
+                        .license(new License().name("MIT").url("https://makariev.com")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Bookstore  Documentation")
+                        .url("https://makariev.com"));
+    }
+
 }
